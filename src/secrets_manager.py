6ce6ab5e2a86f557749d80b427f88b9271dba22f
Norma-99 <normaescg@gmail.com>
2024-07-10 19:20:48 +0200

    Added full pytesting to the functions dor data gathering

--- src/secrets_manager.py
+++ src/secrets_manager.py
@@ -1,19 +1,22 @@
 import boto3
 
 def get_parameter(name):
+    """
+    Retrieves the value of an AWS Systems Manager (SSM) parameter.
+
+    This function uses the Boto3 library to create a client for the AWS Systems Manager (SSM)
+    service, and fetches the value of a specified parameter. It supports decryption of secure
+    parameters.
+
+    Parameters:
+    name (str): The name of the parameter to retrieve.
+
+    Returns:
+    str: The decrypted value of the specified parameter.
+    """
     ssm = boto3.client('ssm', region_name='us-west-2')
     response = ssm.get_parameter(
         Name=name,
         WithDecryption=True
     )
     return response['Parameter']['Value']
-
-
-# # Name of the parameter to retrieve
-# parameter_name = 'GOOGLE_PLACES_API_KEY'
-
-# # Retrieve the parameter
-# api_key = get_parameter(parameter_name)
-
-# # Print the retrieved API key
-# print(f"The API key is: {api_key}")
