5f0c7626237454ddd48a04a147fae2468aaa2761
llmerlos <51518775+Norma-99@users.noreply.github.com>
2024-10-15 20:08:13 +0200

    second relative fix

--- visualization_site/pelicanconf.py
+++ visualization_site/pelicanconf.py
@@ -32,7 +32,7 @@ AUTHOR_FEED_RSS = None
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 
 THEME = "theme/notmyidea"
